5620385563b9aea7844329352448601005c32dbe
varomnrg <varomnrg@gmail.com>
2025-04-24 17:26:42 +0700

    fix deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App;
 use Illuminate\Support\ServiceProvider;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::environment("production")) {
+            URL::forceScheme("https");
+        }
     }
 }
